e652070f75bd9f1ccee8427b1fa83b868e61de14
Ranen Allishaw <ranens.work@gmail.com>
2022-11-30 07:59:12 -0500

    LCD MiniGame - Added Header

--- LCD_MiniGame/LCD_MiniGame.ino
+++ LCD_MiniGame/LCD_MiniGame.ino
@@ -1,9 +1,6 @@
-void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
+/*
+Programmer: Ranen Allishaw
+Date: November 30 2022
+Program: LCD MiniGame
+Resource: https://create.arduino.cc/projecthub/muhamd-magdy/arduino-game-by-lcd-9a3bc2?ref=platform&ref_id=424_trending___&offset=8
+ */
